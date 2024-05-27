1dc2f75374a510fc3e4443c99b21dfe29432beb1
Eshaan Joshi <es.v.joshi@gmail.com>
2024-05-27 19:28:12 -0400

    Second commit

--- main.py
+++ main.py
@@ -4,3 +4,18 @@ s = socket.socket()
 
 print("Socket successfully created")
 
+port = 40674
+
+s.bind(('', port))
+
+print(f"socket binded to {port}")
+
+s.listen(5)
+
+print("socket is listening")
+
+while True:
+    c, addr = s.accept()
+    print ('Got connection from', addr)
+    c.send(b'Thank you for connecting')
+    c.close()
